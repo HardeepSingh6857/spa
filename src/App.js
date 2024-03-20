bd9c92f92a6d8cd273494a625455e9ada4bfe919
Hardeep Singh <iharry6857@gmail.com>
2024-03-20 17:37:51 +0530

    routing by using objects and array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div></div>;
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+//here we are using objects and array 
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <Products /> },
+]);
+
+const App = () => {
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
