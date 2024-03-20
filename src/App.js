3d7caa3869d596b6b64d0c16e13ee1b9eda3cbbf
Hardeep Singh <iharry6857@gmail.com>
2024-03-20 17:40:42 +0530

    routing using react jsx code for routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+
+//here we are using react jsx code for routing
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />}/>
+    <Route path="/products" element={<Products />}/>
+  </Route>
+);
+
 
 //here we are using objects and array 
 const router = createBrowserRouter([
