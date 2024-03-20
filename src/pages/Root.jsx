deaf84e03b2bec2ae9d9ff041f487f5a93191d39
Hardeep Singh <iharry6857@gmail.com>
2024-03-20 18:15:20 +0530

    Added our main navigation inside Root component

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import MainNavigation from '../components/MainNavigation'
 
 const Root = () => {
     return (
         <>
             <h2>Root Layout</h2>
+            <MainNavigation />
             <Outlet />
         </>
     )
